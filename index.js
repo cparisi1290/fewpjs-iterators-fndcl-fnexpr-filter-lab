3a5c9b6cd0dafa78dce1034ee3114a80b3347630
Cassandra <cparisi1290@gmail.com>
2021-03-29 16:05:56 -0400

    commit tree 5a85456a5090b87a2262c3721a7ba1d98bfcc6fb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toUpperCase() === string.toUpperCase()
+    )
+  }
+
+  function fuzzyMatch(drivers, string) {
+    return drivers.filter( possibleMatch =>
+        possibleMatch.toUpperCase().indexOf(string.toUpperCase()) === 0
+        )
+  }
+
+  function matchName(drivers, string) {
+    return drivers.filter( record => record.name === string)
+  }
